04640694cc5ccacbfb938eec659cb702e649b03a
Anant Mathur <anant.mathur007@gmail.com>
2019-10-12 08:50:07 +0530

    Update folder structure

--- routes/index.js
+++ routes/index.js
@@ -4,44 +4,32 @@ const router = express();
 
 // Home page
 router.get('/', async(req,res) =>{
-    return res.render("../views/home",{
-        'base': req.headers.host
-    });
+    return res.render("../views/pages/home");
 });
 
 // Explore Campus page
 router.get('/campus', async(req,res) =>{
-    return res.render("../views/campus",{
-        'base': req.headers.host
-    });
+    return res.render("../views/pages/campus");
 });
 
 // Events page
 router.get('/events', async(req,res) =>{
-    return res.render("../views/events",{
-        'base': req.headers.host
-    });
+    return res.render("../views/pages/events");
 });
 
 // College Updates page
 router.get('/updates', async(req,res) =>{
-    return res.render("../views/updates",{
-        'base': req.headers.host
-    });
+    return res.render("../views/pages/updates");
 });
 
 // Blog Page
 router.get('/blogs', async(req,res) =>{
-    return res.render("../views/blogs",{
-        'base': req.headers.host
-    });
+    return res.render("../views/pages/blogs");
 });
 
 // Team page
 router.get('/team', async(req,res) =>{
-    return res.render("../views/team",{
-        'base': req.headers.host
-    });
+    return res.render("../views/pages/team");
 });
 
 module.exports = router;
