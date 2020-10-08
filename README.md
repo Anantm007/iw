3c7c19bd780b032c3547ebfb9051a598565eb3f9
Anant <anant.mathur007@gmail.com>
2020-10-08 10:44:33 +0530

    update instructions

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # MSI-T Unfiltered
+
 Official website of MSI-T Unfiltered under Inception Wave.
-https://iwmsit.herokuapp.com
+
+## Project Setup
+
+```javascript
+1. Clone the repo
+2. cd iw
+3. npm install
+4. paste the .env file in the root folder
+5. npm run dev
+6. Open the project on http://localhost:3000
+```
+
+## Tech Stack:
+
+- Node.js
+- Express.js
+- MongoDB
+- Dependencies - body-parser, config, dotenv, ejs, express, express-session, fs, mongoose, multer, node-cron, nodemailer, request, uuid, morgan
--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-require('dotenv').config()
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const cron = require("node-cron");
 const instaPosts = require("./routes/cronInstaPosts");
 
 // MongoDB url
-const url = process.env.mongoURI
+const url = process.env.mongoURI;
 
 //Connecting to the database
 mongoose.promise = global.Promise;
-mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true}, (err,db)=> {
-    if(err)
-    console.log(err);
-
-    else
-    console.log('Database Connected');
-});
-mongoose.set('useFindAndModify', false);
+mongoose.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, db) => {
+    if (err) console.log(err);
+    else console.log("Database Connected");
+  }
+);
+mongoose.set("useFindAndModify", false);
 
 // Getting data in json format
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Setting express engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static("views"));
 app.use(express.static("public"));
 
 // routes
-app.use('/', require('./routes/index'));
-app.use('/admin', require('./routes/admin/auth'));
-app.use('/admin/blogs', require('./routes/admin/blogsroutes'));
-app.use('/', require('./routes/admin/contact'));
-
+app.use("/", require("./routes/index"));
+app.use("/admin", require("./routes/admin/auth"));
+app.use("/admin/blogs", require("./routes/admin/blogsroutes"));
+app.use("/", require("./routes/admin/contact"));
 
 // Cronjob to fetch posts from insta API and update database everyday at 2:00 AM
-cron.schedule("0 2 * * *", async() => {
-    console.log('running');
+cron.schedule("0 2 * * *", async () => {
+  console.log("running");
 
-    await instaPosts();
-})
-
+  await instaPosts();
+});
 
 // Starting the server
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Server started on port 3000"); 
+app.listen(process.env.PORT || 3001, () => {
+  console.log("Server started on port 3000");
 });
